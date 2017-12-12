7e8f92a5ec913f28531c2dcb9967db948dbb91aa
Tarciso Braz <tarcisocomp@gmail.com>
2017-12-12 18:17:56 -0300

    Adding parameter passing and check to script.

--- run-spark-scripts.sh
+++ run-spark-scripts.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
 
-FILES=/local/tarciso/masters/experiments/preliminary-exp/preliminary-exp-sample-data/buste-v3a/06_18-24_2017/buste/per_day/*
-for f in $FILES
-do
-    filename=${f##*/} # will drop begin of string upto last occur of `SubStr`
-	date=${filename%_*}  # will drop part of string from last occur of `SubStr` to the end
-	formatted_date=${date//_/-}
-		echo "Processing date $formatted_date ..."
-    time spark-submit --driver-memory 4g /local/tarciso/masters/experiments/preliminary-exp/trips-optimality-exp/evaluate-user-trips-decisions.py $formatted_date $formatted_date  /local/tarciso/masters/experiments/preliminary-exp/preliminary-exp-sample-data/buste-v3a/06_18-24_2017/filtered_od/ /local/tarciso/masters/experiments/preliminary-exp/preliminary-exp-sample-data/buste-v3a/06_18-24_2017/buste/per_day/ http://localhost:5601/otp/ /local/tarciso/masters/experiments/preliminary-exp/trips-optimality-exp/
- 
-done
+if [ "$#" -lt 5 ]; then
+    echo "Wrong number of parameters"
+	echo "Usage:"
+	echo "./run-spark-scripts.sh <script_path> <buste_data_folderpath> <od_matrix_folderpath> <otp_server_url> <results_folderpath>"
 
-echo "Done!"
+else
 
+	script_path=$1
+	buste_data_folderpath=$2
+	od_matrix_folderpath=$3
+	otp_server_url=$4
+	results_folderpath=$5
+
+
+	for f in $buste_data_folderpath/*
+	do
+		filename=${f##*/} # will drop begin of string upto last occur of `SubStr`
+		date=${filename%_*}  # will drop part of string from last occur of `SubStr` to the end
+		formatted_date=${date//_/-}
+			echo "Processing date $formatted_date ..."
+		time spark-submit --driver-memory 4g $script_path $formatted_date $formatted_date $od_matrix_folderpath $buste_data_folderpath $otp_server_url $results_folderpath
+	 
+	done
+
+	echo "Done!"
+
+fi
