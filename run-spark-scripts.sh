5ad365d46bbce13de23c612d29d9e0c89d6610e9
Tarciso Braz <tarcisocomp@gmail.com>
2018-03-26 23:24:52 -0300

    Adjusting code to read BUSTE data folders without _veiculos suffix.

--- run-spark-scripts.sh
+++ run-spark-scripts.sh
@@ -18,8 +18,8 @@ else
 	for f in $buste_data_folderpath/*
 	do
 		filename=${f##*/} # will drop begin of string upto last occur of `SubStr`
-		date=${filename%_*}  # will drop part of string from last occur of `SubStr` to the end
-		formatted_date=${date//_/-}
+		#date=${filename%_*}  # will drop part of string from last occur of `SubStr` to the end
+		formatted_date=${filename//_/-}
 			echo "Processing date $formatted_date ..."
 		time spark-submit --driver-memory 4g $script_path $formatted_date $formatted_date $od_matrix_folderpath $buste_data_folderpath $otp_server_url $results_folderpath
 	 
